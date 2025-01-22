c00096fdd37f8baf477f06a6a406316c62fdae32
LeeJDH <li.alisher0508@mail.ru>
2025-01-22 19:23:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let item = "";
 let selectedItems = [];
+const itemPrices = { 1: 1000, 2: 2000, 3: 3000, 4: 4000, 5: 5000, 6: 6000 };
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -17,22 +17,30 @@ let btn6 = document.getElementById("btn6");
 
 function handleButtonClick(itemNumber) {
     if (selectedItems.includes(itemNumber)) {
+        // Убираем товар из списка, если он уже был выбран
         selectedItems = selectedItems.filter(i => i !== itemNumber);
     } else {
+        // Добавляем товар в список
         selectedItems.push(itemNumber);
     }
     updateMainButton();
 }
 
+function calculateTotal() {
+    return selectedItems.reduce((total, item) => total + itemPrices[item], 0);
+}
+
 function updateMainButton() {
     if (selectedItems.length > 0) {
-        tg.MainButton.setText(`Вы выбрали товары: ${selectedItems.join(", ")}`);
+        const total = calculateTotal();
+        tg.MainButton.setText(`Выбрано: ${selectedItems.join(", ")} | Сумма: ${total} KZT`);
         tg.MainButton.show();
     } else {
         tg.MainButton.hide();
     }
 }
 
+// Привязываем обработчики к кнопкам товаров
 btn1.addEventListener("click", function () {
     handleButtonClick(1);
 });
@@ -52,16 +60,19 @@ btn6.addEventListener("click", function () {
     handleButtonClick(6);
 });
 
+// Обработка события MainButton
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(JSON.stringify(selectedItems));
+    const total = calculateTotal();
+    tg.sendData(JSON.stringify({ items: selectedItems, total }));
 });
 
+// Отображение информации о пользователе
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 usercard.appendChild(p);
 
-// Кнопка для случайного выбора заказов
+// Случайный заказ
 let randomOrderBtn = document.getElementById("randomOrder");
 randomOrderBtn.addEventListener("click", function () {
     let randomItems = [];
