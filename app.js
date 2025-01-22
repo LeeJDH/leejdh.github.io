86038445cfba79adc3b1cdda434973679893b396
LeeJDH <li.alisher0508@mail.ru>
2025-01-22 19:18:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let item = "";
+let selectedItems = [];
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -15,12 +16,20 @@ let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
 function handleButtonClick(itemNumber) {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
+    if (selectedItems.includes(itemNumber)) {
+        selectedItems = selectedItems.filter(i => i !== itemNumber);
     } else {
-        tg.MainButton.setText(`Вы выбрали товар ${itemNumber}!`);
-        item = itemNumber.toString();
+        selectedItems.push(itemNumber);
+    }
+    updateMainButton();
+}
+
+function updateMainButton() {
+    if (selectedItems.length > 0) {
+        tg.MainButton.setText(`Вы выбрали товары: ${selectedItems.join(", ")}`);
         tg.MainButton.show();
+    } else {
+        tg.MainButton.hide();
     }
 }
 
@@ -44,10 +53,24 @@ btn6.addEventListener("click", function () {
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(item);
+    tg.sendData(JSON.stringify(selectedItems));
 });
 
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 usercard.appendChild(p);
+
+// Кнопка для случайного выбора заказов
+let randomOrderBtn = document.getElementById("randomOrder");
+randomOrderBtn.addEventListener("click", function () {
+    let randomItems = [];
+    let allItems = [1, 2, 3, 4, 5, 6];
+    for (let i = 0; i < 3; i++) {
+        let randomIndex = Math.floor(Math.random() * allItems.length);
+        randomItems.push(allItems[randomIndex]);
+        allItems.splice(randomIndex, 1);
+    }
+    selectedItems = randomItems;
+    updateMainButton();
+});
